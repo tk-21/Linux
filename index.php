815fff12912058679bba40b4a3f362a51fd4e689
takuya <83660733+tk-21@users.noreply.github.com>
2021-12-14 17:48:24 +0900

    php

--- index.php
+++ index.php
@@ -8,3 +8,13 @@ $numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 for($i = 0; $i < 10; $i++) {
 	echo $numbers[$i] . PHP_EOL;
 }
+
+$members = [
+	1 => "ichiro",
+	2 => "jiro",
+	3 => "saburo"
+];
+
+foreach($members as $key => $value) {
+	echo sprintf("%d is %s", $key, $value) . PHP_EOL;
+}
