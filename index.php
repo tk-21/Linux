e887ff8cff25efde220c7b8d98267f1e81f2bdfa
takuya <83660733+tk-21@users.noreply.github.com>
2021-12-18 18:10:44 +0900

    php

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ echo $num;
 
 $numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+
 for($i = 0; $i < 10; $i++) {
-	echo $numbers[$i] . PHP_EOL;
-}
+	echo $numbers[$i] . PHP_EOL; }
 
 $members = [
 	1 => "ichiro",
